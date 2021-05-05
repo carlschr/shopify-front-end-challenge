eaedb6b4e38ba715f8b13d5742551fa4d7887218
carlschr <cjcarlson136@gmail.com>
2021-05-05 18:47:39 -0500

    Limit search results to five movies

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,8 +1,8 @@
 import Movie from './Movie';
 
 function MovieList({results}) {
-    const movies = (results) ? results.map(result =>{
-        return <Movie key={result.imdbID} movieData={result}/>
+    const movies = (results) ? results.map((result, i) =>{
+        if (i < 5) return <Movie key={result.imdbID} movieData={result}/>
     }) : <p>No results found.</p>
     return (
         <div className='MovieList'>
