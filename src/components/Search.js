2858db72ee93bab200b022f24512fa5bb864db28
carlschr <cjcarlson136@gmail.com>
2021-05-05 19:48:50 -0500

    Fix label warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 function Search({handleChange}) {
     return (
         <form className='Search'>
-            <label for='movie'>Enter a Movie Title to Make a Nomination</label>
+            <label htmlFor='movie'>Enter a Movie Title to Make a Nomination</label>
             <input onChange={handleChange} id='movie' type='text' />
         </form>
     )
